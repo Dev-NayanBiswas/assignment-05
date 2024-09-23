0fde2178790587067ff026ceb05e4e76e66999cb
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2024-09-24 05:08:48 +0600

    code refactoring Done

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-const history = document.querySelector('.history-container');
-    document.querySelector('#history').addEventListener('click', activeHistory)
-const donation = document.querySelector('.card_container');
-    document.querySelector('#donate').addEventListener('click', activeDonation)
+function main() {
+    const cards = document.querySelector('.donation_card_container').children;
+
+    for (let child of cards) {
+        let donationBox = child.getElementsByTagName('span')[0];
+        child.addEventListener('click', (e) => global(e, donationBox))
+    }
+}
 
 
+main();
 
 
-const cardOne = document.querySelector('.donation__cardOne');
-cardOne.addEventListener('click',function(e){global(e,'.noakhali')})
-
-const cardTwo = document.querySelector('.donation__cardTwo');
-cardTwo.addEventListener('click',function(e){global(e,'.feni')})
-
-const cardThree = document.querySelector('.donation__cardThree');
-cardThree.addEventListener('click',function(e){global(e,'.quota')})
-
-
-
-function global(e, name){
-    const cross ="https://media.lordicon.com/icons/wired/lineal/38-error-cross-simple.json"; 
-    const empty ="https://media.lordicon.com/icons/wired/lineal/2465-restriction.json";
+function global(e, name) {
+    const cross = "https://media.lordicon.com/icons/wired/lineal/38-error-cross-simple.json";
+    const empty = "https://media.lordicon.com/icons/wired/lineal/2465-restriction.json";
     const recharge = "https://media.lordicon.com/icons/wired/lineal/2531-recurring-cash.json";
     const success = "https://media.lordicon.com/icons/wired/lineal/291-coin-dollar.json";
 
     const stringTotal = document.querySelector('.totalAmount');
     let availableTaka = parseInt(stringTotal.textContent);
     const heading = e.currentTarget.querySelector('h4 span').textContent;
-    const staticBox = document.querySelector(name);
+    const staticBox = name;
     let donateBoxTotal = parseInt(staticBox.textContent);
 
     console.log(donateBoxTotal)
-    
-    if(e.target.nodeName==='BUTTON'){
+
+    if (e.target.nodeName === 'BUTTON') {
         const input = e.currentTarget.querySelector('input');
         const inputValue = input.value;
         const inputToNumber = parseInt(inputValue);
-        
-        if(inputValue){
-            let trimData =inputValue.trim()
-            
-            if(inputToNumber === 0 || inputToNumber < 0 || trimData === ""){
-                
+
+        if (inputValue) {
+            let trimData = inputValue.trim()
+
+            if (inputToNumber === 0 || inputToNumber < 0 || trimData === "") {
+
                 input.value = '';
-                activateModal('Wrong!','Enter a valid Amount',"Invalid",empty)
+                activateModal('Wrong!', 'Enter a valid Amount', "Invalid", empty)
                 return;
             }
 
             let isNumber = true;
 
-            for(let i=0; i < trimData.length; i++){
+            for (let i = 0; i < trimData.length; i++) {
                 let char = trimData[i];
-                if(isNaN(char)){
-                    activateModal('Invalid!','Enter a valid Amount',"Error",cross)
+                if (isNaN(char)) {
+                    activateModal('Invalid!', 'Enter a valid Amount', "Error", cross)
                     isNumber = false;
                     input.value = '';
                     return;
@@ -59,7 +53,7 @@ function global(e, name){
             }
 
 
-            if(isNumber && availableTaka >= inputToNumber){
+            if (isNumber && availableTaka >= inputToNumber) {
                 console.log(inputToNumber)
                 donateBoxTotal += inputToNumber;
                 staticBox.textContent = donateBoxTotal;
@@ -67,65 +61,68 @@ function global(e, name){
                 updateHistory(forHistory);
                 availableTaka -= inputToNumber;
                 stringTotal.textContent = availableTaka;
-                activateModal('Congrats!!','You have donated for Humankind',"Success",success)
-                input.value = '';    
+                activateModal('Congrats!!', 'You have donated for Humankind', "Success", success)
+                input.value = '';
                 return;
-                
-            }else{
+
+            } else {
                 input.value = ''
-                activateModal('Exceed !!','insufficient Balance recharge or Try another amount','Try Again',recharge)
+                activateModal('Exceed !!', 'Insufficient Balance', 'Try Again', recharge)
                 return;
             }
-        }else{
-            activateModal('Sorry!','Enter a valid Amount',"Wrong Input",empty)
+        } else {
+            activateModal('Sorry!', 'Enter a valid Amount', "Wrong Input", empty)
             return;
         }
     }
-    
+
 }
 
 
 
 
-function activateModal(heading,hint,success,icon){
+function activateModal(heading, hint, success, icon) {
 
-    console.log(heading,hint,success,icon)
-    document.getElementsByTagName('lord-icon')[0].setAttribute('src',"")
-    document.getElementsByTagName('lord-icon')[0].setAttribute('src',icon)
-    document.querySelector('.modalHeading').innerText=heading;
-    document.querySelector('.modalHint').innerText=hint;
-    document.querySelector('.success').innerText=success;
+    console.log(heading, hint, success, icon)
+    document.getElementsByTagName('lord-icon')[0].setAttribute('src', "")
+    document.getElementsByTagName('lord-icon')[0].setAttribute('src', icon)
+    document.querySelector('.modalHeading').innerText = heading;
+    document.querySelector('.modalHint').innerText = hint;
+    document.querySelector('.success').innerText = success;
     document.querySelector('.overlay').classList.remove('hidden')
     document.querySelector('.myModal').classList.remove('hidden')
 }
 
 
 
-document.querySelector('.modalClose').addEventListener('click',closeModal)
-function closeModal(){
-    document.getElementsByTagName('lord-icon')[0].setAttribute('src',"")
+document.querySelector('.modalClose').addEventListener('click', closeModal)
+function closeModal() {
+    document.getElementsByTagName('lord-icon')[0].setAttribute('src', "")
     document.querySelector('.overlay').classList.add('hidden')
     document.querySelector('.myModal').classList.add('hidden')
 }
 
 
-function updateHistory(msg){
+function updateHistory(msg) {
     let time = new Date();
     const history = document.querySelector('.history-container');
     const article = document.createElement('article');
     article.setAttribute('class', "record flex flex-col rounded-xl border-[1px] border-gray-200 px-10 py-5 shadow-lg");
-        article.innerHTML = `
+    article.innerHTML = `
         <p class="taka text-2xl text-gray-700">${msg}</p>
         <p class="time text-xl font-light italic">${time}</p>
         `
-        history.append(article)
+    history.append(article)
 }
 
 
+const history = document.querySelector('.history-container');
+document.querySelector('#history').addEventListener('click', activeHistory)
+const donation = document.querySelector('.card_container');
+document.querySelector('#donate').addEventListener('click', activeDonation)
 
 
-
-function activeHistory(){
+function activeHistory() {
     document.querySelector('#donate').classList.remove('bg-lime-600')
     document.querySelector('#donate').classList.add('bg-green-50')
     document.querySelector('#donate').classList.remove('text-gray-200')
@@ -133,11 +130,10 @@ function activeHistory(){
 
     document.querySelector('#history').classList.add('bg-lime-600')
     document.querySelector('#history').classList.add('text-gray-200')
-    const history = document.querySelector('.history-container');
 
-    if(history.childElementCount > 1){
+    if (history.childElementCount > 1) {
         document.querySelector('.noDonation').classList.add('hidden')
-    }else{
+    } else {
         document.querySelector('.noDonation').classList.remove('hidden')
     }
     donation.classList.add('hidden');
@@ -145,11 +141,12 @@ function activeHistory(){
 
 }
 
-function activeDonation(){
+function activeDonation() {
+
     document.querySelector('#donate').classList.add('bg-lime-600')
     document.querySelector('#donate').classList.add('text-gray-200')
 
-    
+
     document.querySelector('#history').classList.remove('bg-lime-600')
     document.querySelector('#history').classList.remove('text-gray-200')
     history.classList.add('hidden');
